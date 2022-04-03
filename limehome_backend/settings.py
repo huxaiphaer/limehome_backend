6863af7cfc3ee78affa2ff00fb21ee58f5f80109
Lutaaya Idris <lutaaya.idris@captiq.com>
2022-04-03 14:46:37 +0300

    Add db connections

--- limehome_backend/settings.py
+++ limehome_backend/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,6 +87,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': dj_database_url.config(
+        conn_max_age=600, ssl_require=False, default=os.getenv('DATABASE_URL'))
 }
 
 
